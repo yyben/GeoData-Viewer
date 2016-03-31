f7e3058f2f919d70881b789ddcc80d7ccf26b0a2
Yao-Yu Yang <yyben@Yao-Yus-MacBook-Pro.local>
2016-03-31 13:15:26 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-main.js
+var fname="data1.tsv", fpath="./p2data/";
+var times=[],dataRainfall=[],dataGndWater=[],dataDist=[];//array
+var objRainfall=[], objGndWater=[], objDist=[];//e.g. {cdate: 2012/6/5 0:00,val:-55}
+var dateformat=d3.time.format("%Y/%m/%d %H:%M");
+var labelDateFormat=d3.time.format("%Y/%m/%d ");
+parseData();
+function parseData(){
+	d3.text(fpath+fname,function(text){
+		d3.csv.parseRows(text,function(d,i){
+			var tTmp=dateformat.parse(d[0]);
+			times.push(tTmp);
+			dataRainfall.push(parseFloat(d[1]));
+			dataGndWater.push(parseFloat(d[2]));
+			dataDist.push(parseFloat(d[3]));
+			objRainfall.push({cdate:tTmp,val:parseFloat(d[1])});
+			objGndWater.push({cdate:tTmp,val:parseFloat(d[2])});
+			objDist.push({cdate:tTmp,val:parseFloat(d[3])});	
+		});
+		console.log(objRainfall);
+
+
+	});
+
+
+}
